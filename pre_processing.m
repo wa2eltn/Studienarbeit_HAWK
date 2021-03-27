aa15bfe1df53fdc9a245b88e95b132d86eed93e8
Wael Amaria <amaria.wael@gmail.com>
2021-03-27 23:20:46 +0100

    pca through hsv conversion and hue region selection ( after studying its histogram )

--- pre_processing.m
+++ pre_processing.m
@@ -1,54 +1,12 @@
-clear all;
+clewr all;
 close all;
 clc;
-           
-filename= 'E:\HAWK\Database\dataset_original\c5yvn32dzg-2\Photos\C4P29H2.jpg'; 
+
+
+filename= 'E:\HAWK\Database\dataset_original\c5yvn32dzg-2\Photos\C2P23E2.jpg'; 
 rgb = imread(filename);
 
-% 
-%  [filename,path] = uigetfile('*.jpg',...
-%                'Select an image file','E:\HAWK\Database\dataset_organized\healthy')
-%            
-% c = imread([path filesep filename]);
-
-
-[w,h]=size(rgb,1,2);
 figure;
-imshow(rgb,[]);
- hsv=rgb2hsv(rgb);
+imshow(rgb);
 
-% figure;
-% hi=histogram(hsv(:,:,3));
-
-for i=1:w
-    for k=1:h
-        
-        if((hsv(i,k,1)>0.5)||(hsv(i,k,1)<0.09)||(hsv(i,k,2)<0.2)||(hsv(i,k,3)>0.8))
-            hsv(i,k,1)=0;
-            hsv(i,k,2)=0;
-            hsv(i,k,3)=0;
-        end
-        
-        
-    end
-end
-
-rgb = hsv2rgb(hsv);
-figure;
-imshow(rgb,[]);
-
-
- rgb(:,:,2) = bwareaopen(rgb(:,:,2), 200);
-g=rgb2gray(rgb);
-
-%  g = edge(g,'Prewitt');
-
-% se = strel('disk',20);
-% g=imclose(g,se);
-figure;
-imshow(g,[]);
-
-
-
-
- 
+rgb=pre_processing(rgb);
---
+++ single_image_pre_processing.m
@@ -0,0 +1,22 @@
+function [rgb_out] = single_image_pre_processing(rgb)
+
+hsv = rgb2hsv(rgb);
+
+BW = (hsv(:,:,1) >= 00 ) & (hsv(:,:,1) <= 0.215) & ...
+    (hsv(:,:,2) >= 0.3 ) & (hsv(:,:,2) <= 1);
+
+% figure;imshow(BW);
+
+BW = bwareaopen(BW, 200);
+BW=imfill(BW,'holes');
+
+% figure;imshow(BW);
+
+rgb_out = rgb;
+rgb_out(repmat(~BW,[1 1 3])) = 0;
+
+end
+
+
+
+ 
