a92d53518bf5b15d3ddab17dd7a3b0c0db0886d3
Wael Amaria <amaria.wael@gmail.com>
2021-03-29 20:24:42 +0200

    error corrected .. i insteadof 1

--- pre_processing.m
+++ pre_processing.m
@@ -19,12 +19,15 @@ photos_path = uigetdir('dataset_original\c5yvn32dzg-2\Photos','Please select the
 filelist = dir(fullfile(photos_path, '**\*.*'));  %get list of files and folders in any subfolder
 filelist = filelist(~[filelist.isdir]); 
 
+photos_out = uigetdir('dataset_trated\c5yvn32dzg-2\Photos','Please select the photos folder');
+
 for i=1:size(filelist,1)
     
- filename=[ filelist(1).folder filesep filelist(1).name];
+ filename=[ filelist(1).folder filesep filelist(i).name];
  rgb=imread(filename);
  rgb=single_image_pre_processing(rgb);
  
+ filename=[ photos_out filesep filelist(i).name];
  imwrite(rgb,filename)
    
  disp(strjoin(["Photo " string(i) "/" string(size(filelist,1)) " Photo name : "  filelist(i).name],...
