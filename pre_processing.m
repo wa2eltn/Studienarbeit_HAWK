85f672e3207271987378095a9945ac4df9725d5c
wa2eltn <amaria.wael@gmail.com>
2021-04-04 22:06:21 +0200

    try catch added with error_log

--- pre_processing.m
+++ pre_processing.m
@@ -19,19 +19,23 @@ photos_path = uigetdir('dataset_original\c5yvn32dzg-2\Photos','Please select the
 filelist = dir(fullfile(photos_path, '**\*.*'));  %get list of files and folders in any subfolder
 filelist = filelist(~[filelist.isdir]); 
 
-photos_out = uigetdir('dataset_trated\c5yvn32dzg-2\Photos','Please select the photos folder');
-
+err_log=[];
 for i=1:size(filelist,1)
-    
+    try
  filename=[ filelist(1).folder filesep filelist(i).name];
  rgb=imread(filename);
  rgb=single_image_pre_processing(rgb);
  
- filename=[ photos_out filesep filelist(i).name];
  imwrite(rgb,filename)
    
  disp(strjoin(["Photo " string(i) "/" string(size(filelist,1)) " Photo name : "  filelist(i).name],...
         ''))
-      
+    catch
+ 
+        
+        err_log=[err_log ; strjoin(["Photo " string(i) "/" string(size(filelist,1)) " Photo name : "  filelist(i).name],...
+        '')];
+    
+    end
 end
 
